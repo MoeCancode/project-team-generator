bc2ad342bbc6b3ab37a8068bda0b96ff76fa534c
M A Razvi <abuzarflw@gmail.com>
2022-04-24 01:54:05 -0700

    Update everything

--- js/index.js
+++ js/index.js
@@ -1,39 +1,44 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const managerJS = require('./manager');
-const employeejs = require('./employee');
-const internjs = require('./intern');
-const engineerjs = require('./engineer');
+const managerJS = require("./manager");
+const employeejs = require("./employee");
+const internjs = require("./intern");
+const engineerjs = require("./engineer");
 var htmlString = "";
 
-
-
-
 function startProject() {
-    //Call function that inquires for manager details
-    htmlString = htmlString + managerJS();
-    addEmployee();
-    
-    
+  //Call function that inquires for manager details
+  htmlString = htmlString + managerJS();
+  addEmployee();
 }
 
 function addEmployee() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "WOULD YOU LIKE TO ADD SOMEONE ELSE TO YOUR PROJECT? ",
-            name: "choose",
-            choices: ["ADD AN ENGINEER", "ADD AN INTERN", "TEAM IS COMPLETE (EXIT)"],
-        }
-    ]).then(answer => {
-        if(answer.choose == "ADD AN ENGINEER") {
-            engineerjs();
-        }
-        else if (answer.choose == "ADD AN INTERN") {
-            internjs();
-        }
-    })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "WOULD YOU LIKE TO ADD SOMEONE ELSE TO YOUR PROJECT? ",
+        name: "choose",
+        choices: [
+          "ADD AN ENGINEER",
+          "ADD AN INTERN",
+          "TEAM IS COMPLETE (EXIT)",
+        ],
+      },
+    ])
+    .then((answer) => {
+      if (answer.choose == "ADD AN ENGINEER") {
+        // engineerjs();
+        console.log("Engineer");
+        addEmployee();
+      } else if (answer.choose == "ADD AN INTERN") {
+        //   internjs();
+        console.log("Intern");
+        addEmployee();
+      } else {
+        compileHTML();
+      }
+    });
 }
 
 startProject();
-
--- js/intern.js
+++ js/intern.js
@@ -15,3 +15,32 @@ class Intern extends EmployeeClass {
         console.log(`My role is ${this.role}`);
     }
 }
+
+function inquireIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "ENTER Intern NAME: ",
+            name: "inputName",
+        },
+        {
+            type: "input",
+            message: "ENTER Intern ID: ",
+            name: "inputID",
+        },
+        {
+            type: "input",
+            message: "ENTER Intern EMAIL: ",
+            name: "inputEmail",
+        },
+        {
+            type: "input",
+            message: "ENTER Intern SCHOOL NAME: ",
+            name: "inputSchool",
+        }
+    ]).then(answers => {
+        const newIntern = new Intern(answers.inputName, answers.inputID, answers.inputEmail, answers.inputSchool);
+        console.log(newIntern);
+        makeInternHTML(newIntern);
+    })
+}
