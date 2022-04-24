67cee1e1217b180c532f96977922f71b78226c16
M A Razvi <abuzarflw@gmail.com>
2022-04-24 01:06:42 -0700

    Make addEmplyee function that can be called whenever a new employee/manager is added

--- js/index.js
+++ js/index.js
@@ -12,12 +12,27 @@ var htmlString = "";
 function startProject() {
     //Call function that inquires for manager details
     htmlString = htmlString + managerJS();
+    addEmployee();
     
     
 }
 
 function addEmployee() {
-
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "WOULD YOU LIKE TO ADD SOMEONE ELSE TO YOUR PROJECT? ",
+            name: "choose",
+            choices: ["ADD AN ENGINEER", "ADD AN INTERN", "TEAM IS COMPLETE (EXIT)"],
+        }
+    ]).then(answer => {
+        if(answer.choose == "ADD AN ENGINEER") {
+            engineerjs();
+        }
+        else if (answer.choose == "ADD AN INTERN") {
+            internjs();
+        }
+    })
 }
 
 startProject();
