66d9d801b3096f85ef638e9fcda6bcf4f364bf14
M A Razvi <abuzarflw@gmail.com>
2022-04-25 01:42:00 -0700

    Change intern template literal. Style it

--- js/intern.js
+++ js/intern.js
@@ -47,13 +47,13 @@ function inquireIntern() {
 
 function makeInternHTML(data) {
     var internString = `
-    <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-  <div class="card-header"><i class="fa-solid fa-face-glasses fa-xl"></i> ${data.role}</div>
+    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header"><i class="fa-solid fa-glasses fa-xl"></i> ${data.role}</div>
   <div class="card-body">
     <h5 class="card-title">${data.name}</h5>
     <p class="card-text">ID: ${data.id}</p>
     <p class="card-text">EMAIL: <a href = "" >${data.email}</a></p>
-    <p class="card-text">OFFICE #: ${data.school}</p>
+    <p class="card-text">SCHOOL NAME: ${data.school}</p>
   </div>
 </div>
   `
