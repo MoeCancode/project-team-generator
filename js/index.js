ad0ec79bfbe9bfe7edd8cf3a426b6eed207f2fbf
M A Razvi <abuzarflw@gmail.com>
2022-04-25 01:01:43 -0700

    Compile html string and write to new html file

--- js/index.js
+++ js/index.js
@@ -4,36 +4,36 @@ const managerjs = require("./manager");
 const employeejs = require("./employee");
 const internjs = require("./intern");
 const engineerjs = require("./engineer");
+const path = require("path");
 var htmlString = "";
 
 function startProject() {
   //Call function that inquires for manager details
-  managerjs().then(managerString => {
+  managerjs().then((managerString) => {
     htmlString = htmlString + managerString;
-    
-    addOrNo();
 
-    
+    addOrNo();
   });
-  
 }
 
 function addOrNo() {
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "WOULD YOU LIKE TO ADD ANOTHER EMPLOYEE?",
-      name: "yesno",
-      choices: ["YES", "NO"]
-    }
-  ]).then(answer => {
-    if(answer.yesno == "YES") {
-      addEmployee();
-    }
-    else {
-      console.log(htmlString);
-    }
-  })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "WOULD YOU LIKE TO ADD ANOTHER EMPLOYEE?",
+        name: "yesno",
+        choices: ["YES", "NO"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.yesno == "YES") {
+        addEmployee();
+      } else {
+        compileHtml();
+        // compileCss();
+      }
+    });
 }
 
 function addEmployee() {
@@ -43,27 +43,55 @@ function addEmployee() {
         type: "list",
         message: "DO YOU WANT TO ADD AN ENGINEER OR AN INTERN? ",
         name: "choose",
-        choices: [
-          "ADD AN ENGINEER",
-          "ADD AN INTERN",
-        ],
+        choices: ["ADD AN ENGINEER", "ADD AN INTERN"],
       },
     ])
     .then((answer) => {
       if (answer.choose == "ADD AN ENGINEER") {
-        engineerjs().then(engineerString => {
-            htmlString = htmlString + engineerString;
-            addOrNo();
-        })
-      } 
-      else if (answer.choose == "ADD AN INTERN") {
-        internjs().then(internString => {
-            htmlString = htmlString + internString;
-            addOrNo();
-        })
-        
-      } 
+        engineerjs().then((engineerString) => {
+          htmlString = htmlString + engineerString;
+          addOrNo();
+        });
+      } else if (answer.choose == "ADD AN INTERN") {
+        internjs().then((internString) => {
+          htmlString = htmlString + internString;
+          addOrNo();
+        });
+      }
     });
+}
+
+function compileHtml() {
+  var completeString = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+
+    <title>Team Generator</title>
+</head>
+<body>
+    <div class="heading">
+        <h1>Go Team!</h1>
+    </div>
+
+    <div class="contents">
+    ${htmlString}
+    </div>
+
+
+    <script src="./js/script.js"></script>
+</body>
+</html>
+    `;
+
+    fs.writeFileSync(path.join(process.cwd(), "index.html"), completeString);
 }
 
 startProject();
